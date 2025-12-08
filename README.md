5cf2152b08855bd4a05559508b3f68a0dc36992a
knutsenr <knutsenrowan@gmail.com>
2025-12-07 19:20:51 -0500

    functioning title search, swapped articles.html format, other modifications

--- README.md
+++ README.md
@@ -3,10 +3,15 @@ Rowan Knutsen 2025.
 Github container for my project in the 2025 introduction to web development for learning designers course. 
 
 ## This Commit
-- **Modified |** Added animations - updated link hovers. Articles in article list now only show description on hover. 
-- **Issues |** Still having PDF issues. The ways that it occurs to me to use remove() have not been doing exactly what I wanted so the current state is reverted to what it was before. For some reason hovering over an article reveals the description for the listing below it, not the one hovered. 
-- **Note |** Sorry I submitted this week's work without realizing I either forgot to push or the push had not gone through! My bad.
-- **NEXT |** Find way to remove() articles. Make search function work. 
+- **Added |** Added search function
+  - Titles will fill in as autocorrect based on user input
+  - Searching by title will limit only applicable articles
+- **Modified |** 
+  - Changed the new article select code from addEventListener to an onclick in the <a> attr. This is cleaner, stops it from running and throwing error each time one clicks anywhere and not just on the link. 
+  - articles.html no longer uses section:target hide/block since there are now 3 sections. Instead, what is shown is controlled by switchPage() in script.js
+- **Issues |** When one hovers over the article list, it opens the description for the article below not the one being hovered. I still have not fixed this. 
+- **Note |** The Forest Management Plan PDF is too large to load properly but all of the rest open in time.
+- **NEXT |** Make search function work. 
 
 ## Last Commits
 - Added relevant PDF folder. Fixed some links.
@@ -19,16 +24,19 @@ Github container for my project in the 2025 introduction to web development for 
 - Created HTML container which will display each of these articles on the articles page of the site 
 - Created extra file for articles.js and article-pdf.html to sparate pages then deleted them when I could not get the js to reference different html files in the same js file.
 - Articles page is currently a grid of different article titles made with Javascript in script.js. This works. I am working on getting the PDFs to display in the page when the link is pressed.
+- Description appears on hover, improved link hover animation.
+- Added animations - updated link hovers. Articles in article list now only show description on hover. 
+- Fixed the loading pdf issue! Now clears all children of getElementById before adding a new one.
 
 ## Pages and Navigation
 ### Files
 **index.html** contains the home page.
 
-**articles.html** contains all articles + example article page.
+**articles.html** contains all articles + single article pages.
 
 **style.css** contains all css for project.
 
-**script.js** contians all javascript for project. articleList.js is currently not interacting with the program so the article array is housed in script.js.
+**script.js** contians all javascript for project. articleList.js is currently not interacting with the program so the article array is also housed in script.js.
 
 ### Navigation
 From Home one can use the Articles selector at the top of the page OR the Articles button under the Resources section.
@@ -38,5 +46,3 @@ The Articles page lists a series of articles selected from a javascript list. Al
 Next:
 - Functional search bar
 - Your Files drop down
-- Speed up PDFs 
-- make listenerevent only go off for links
--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
 // array of article attributes
-const artInfo = [
+const articleData = [
     {
         title: "Buffering Shapefiles in QGIS",
         author: "NYU DataServices",
-        link: "articles.html#single",
         description: "A written guide on buffering shapefiles in open-source GIS software GIS.",
         pdf: "\\pdf_docs\\Buffering Shapefiles in QGIS.pdf"
     },
     {
         title: "Clipping Shapefiles in QGIS",
         author: "NYU DataServices",
-        link: "articles.html#single",
         description: "A written guide on clipping shapefiles in open-source GIS software GIS.",
         pdf: "\\pdf_docs\\Clipping Shapefiles in QGIS.pdf"
     },
     {
         title: "Downloading OpenStreetMap data using QGIS",
         author: "NYU DataServices",
-        link: "articles.html#single",
         description: "A written guide on how to download the OpenStreetMap basemap in open-source GIS software QGIS",
         pdf: "\\pdf_docs\\Downloading OpenStreetMap data using QGIS.pdf"
     },
     {
         title: "FM-1 An Introduction to Forest Management Plans",
-        link: "articles.html#single",
         author: "Rural Action",
         description: "A short overview of what a forest management plan looks like through Rural Action Non-Profit's forestry department.",
         pdf: "\\pdf_docs\\FM-1-An-Introduction-to-Forest-Management-Plans.pdf"
@@ -31,35 +27,63 @@ const artInfo = [
     {
         title: "Forest Farmers Handbook 2023.",
         author: "Rural Action",
-        link: "articles.html#single",
         description: "Rural Action's complete Forest Farmer's Handbook, First Edition written in 2023.",
         pdf: "\\pdf_docs\\Forest-Farmers-Handbook-2023-Final-1.pdf"
     },
     {
         title: "How to Edit Attribute Tables in QGIS",
         author: "NYU DataServices",
-        link: "articles.html#single",
         description: "A written guide on how to edit an attribute file in open-source GIS software QGIS.",
         pdf: "\\pdf_docs\\How to Edit Attribute Tables in QGIS.pdf"
     },
     {
         title: "Merging Shapefiles in QGIS",
         author: "NYU DataServices",
-        link: "articles.html#single",
         description: "A written guide on how to merge shapefiles in open-source GIS software QGIS.",
         pdf: "\\pdf_docs\\Merging Shapefiles in QGIS.pdf"
     }
 ];
 
-// find relevant ids in articles.html
+// FIND relevant ids in articles.html
 const gallery = document.getElementById('art-gallery');
 const single = document.getElementById('single-article');
+const results = document.getElementById('searched-gallery');
+switchPage('page-show-all', 'page-show-searched', 'page-show-single');
 
-// when article link is clicked read in article infor for pdf and add styling 
-const current_id = "x";
-addEventListener("click", function (e) {
-    const result = artInfo.find(({ title }) => title === event.target.id);
+// WHEN ON ALL ARTICLE LIST PAGE read array into 'art cards' to be displayed as article list
+articleData.forEach((element) => createArtCard(element, gallery));
+function createArtCard(entry, output) {
+    const artDiv = document.createElement('div');
+    // This class could be used in CSS to style each tree’s card uniformly.
+    artDiv.className = 'article-list';
 
+    const artLink = document.createElement('a');
+    artLink.id = entry.title;
+    artLink.setAttribute("onclick", "articleSelected(this)");
+    artLink.href = "articles.html#page-show-all";
+    artLink.textContent = entry.title;
+    artDiv.appendChild(artLink);
+
+    const artAuth = document.createElement('h4');
+    artAuth.textContent = " by " + entry.author;
+    artDiv.appendChild(artAuth);
+
+    const artDesc = document.createElement('p');
+    artDesc.textContent = entry.description;
+    artDiv.appendChild(artDesc);
+
+    output.appendChild(artDiv);
+};
+
+// WHEN ARTICLE IS SELECTED check for currently stored article, purge, create new article
+function articleSelected() {
+    while (single.firstChild) {
+        single.removeChild(single.firstChild);
+        console.log("here");
+    }
+    const result = articleData.find(({ title }) => title === event.target.id);
+
+    // CREATE the page for selected PDF [formerly createPage(result)]
     const singlePDF = document.createElement('object');
     singlePDF.data = result.pdf;
     singlePDF.type = "application/pdf";
@@ -68,35 +92,44 @@ addEventListener("click", function (e) {
     singlePDF.width = "100%";
 
     single.appendChild(singlePDF);
-    // singlePDF.remove();
-    // console.log(event.target.id);
-});
+    switchPage('page-show-single', 'page-show-all', 'page-show-searched');
+}
 
-// read array into 'art cards' to be displayed as article list
-artInfo.forEach(createArtCard);
-function createArtCard(art) {
-    const artDiv = document.createElement('div');
-    // This class could be used in CSS to style each tree’s card uniformly.
-    artDiv.className = 'article-list';
+// SEARCH FUNCTION CODE
+function searchTitle() {
+    var input = document.getElementById('searchTitle');
+    filter = input.value.toUpperCase();
 
-    const artLink = document.createElement('a');
-    artLink.id = art.title;
-    artLink.href = art.link;
-    artLink.textContent = art.title;
-    artDiv.appendChild(artLink);
+    const temp = document.createElement("div");
+    articleData.forEach((element) => {
+        console.log("here?");
+        if (element.title.toUpperCase().indexOf(filter) > -1) {
+            console.log("match");
+            createArtCard(element, results);
+        }
+    });
 
-    const artAuth = document.createElement('h4');
-    artAuth.textContent = " by " + art.author;
-    artDiv.appendChild(artAuth);
-
-    const artDesc = document.createElement('p');
-    artDesc.textContent = art.description;
-    artDiv.appendChild(artDesc);
-
-    gallery.appendChild(artDiv);
+    // for (i = 0; i < articleData.length; i++) {
+    //     console.log("here");
+    //     a = temp[i].getElementsByTagName('a')[0];
+    //     txt = a.textContent;
+    //     if (txt.toUpperCase().indexOf(filter) <= -1) {
+    //         temp[i].removeChild();
+    //     }
+    // }
+    console.log(input.value);
+    switchPage('page-show-searched', 'page-show-all', 'page-show-single');
 };
 
-// goal = make article desc appear on hover.
+// REPLACE CSS section block\hide if needed
+function switchPage(shown, hidden, third) {
+    document.getElementById(shown).style.display = 'block';
+    document.getElementById(hidden).style.display = 'none';
+    document.getElementById(third).style.display = 'none';
+    return false;
+}
+
+// goal = make article desc appear on hover. half pts to knut wtf is this problem.
 
 /*
 const gal = document.getElementById('single-article');
@@ -120,4 +153,14 @@ function createArticle(x) {
 
     gal.appendChild(singleDiv);
 };
+    
+// when article link is clicked read in article info for pdf and add styling 
+// addEventListener("click", function (e) {
+//     while (single.firstChild) {
+//         single.removeChild(single.firstChild);
+//         // console.log("here");
+//     }
+//     const result = articleData.find(({ title }) => title === event.target.id);
+//     createPage(result);
+// });
 */
